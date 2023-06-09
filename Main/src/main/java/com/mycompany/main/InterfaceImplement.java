9890f3d3598202df240bca9fee9965275a5abbce
GithmiSenani <135831931+GithmiSenani@users.noreply.github.com>
2023-06-09 23:14:30 +0530

    Lab05-Q1
    Added the question number 1 of lab sheet 5

---
+++ Main/src/main/java/com/mycompany/main/InterfaceImplement.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.main;
+
+
+public class InterfaceImplement implements MyFirstInterface
+{
+
+    @Override
+    public void display() {
+        System.out.println("x");
+    }
+
+    @Override
+    public void display1() {
+        System.out.println("y");
+        // can't to change the variable value, because these variables are final variables.
+    }
+    
+}
---
+++ Main/src/main/java/com/mycompany/main/Main.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.main;
+
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+       InterfaceImplement obj=new InterfaceImplement();
+        obj.display();
+        obj.display1();
+
+    }
+}
